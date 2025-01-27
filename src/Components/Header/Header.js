f968c3ab79009f3bd1f8ed6914de2d8ae31a9314
Opeyemi-Opj <adenubijethro@yahoo.com>
2025-01-27 10:53:15 +0100

    Add files via upload

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='text-blue-600'>hello world</h1>
+      <h1 className="font-cormorant text-3xl">This is Cormorant Garamond</h1>
+     <p className="font-raleway text-lg">This is Raleway</p>
+
+    </div>
+  )
+}
+
+export default Header
