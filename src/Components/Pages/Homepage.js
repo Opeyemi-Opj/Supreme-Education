b4e3e8164dd2e241b08f9f4231266fd0b955a09d
Opeyemi-Opj <adenubijethro@yahoo.com>
2025-01-27 16:02:06 +0100

    Update Homepage

--- src/Components/Pages/Homepage.js
+++ src/Components/Pages/Homepage.js
@@ -9,7 +9,7 @@ const Homepage = () => {
              <p class="font-raleway text-[#292D32] text-[18px] leading-[40px] pl-[20%] pr-[20%]">Supreme Education Foundation School is a centre of excellence designed to provide world-class education in an exciting learning environment for students between the ages of 3 months to 18 years. </p> 
              <button className='font-raleway text-[#FFFFFF] text-[14px] font-medium leading-[16.44px] 
          bg-[#006011] border border-[#C6C5C5] rounded-[8px] px-[24px] py-[12px] items-center gap-5 transition duration-300
-        hover:bg-[#C8352E] '>Learm more</button>
+        hover:bg-[#C8352E] '>Learm more now</button>
         </div>
         </div>
       
